e477ee77c860233f25bad4f1f4f97f4348c16694
lattey <l4tt1haga@gmail.com>
2025-08-02 21:01:24 +0200

    adding new color scheme

--- lua/config/plugins/colorscheme.lua
+++ lua/config/plugins/colorscheme.lua
@@ -1,11 +1,17 @@
 return {
     {
-      "p00f/alabaster.nvim",
+      "d00h/nvim-rusticated",
       config = function()
-        vim.cmd.colorscheme "alabaster";
+        vim.cmd.colorscheme "rusticated";
 
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
+        -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" });
+        -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" });
       end
     },
+    -- {
+    --   "lifepillar/vim-solarized8",
+    --   config = function()
+    --     vim.cmd.colorscheme "solarized8_low";
+    --   end
+    -- },
 }
